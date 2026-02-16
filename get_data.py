836ccdad52b0c1e6e08ee168534e327a5ef39421
jagadeeshvarma927 <jagadeeshvarma927@gmail.com>
2026-02-16 11:38:05 +0530

    changes addeed

--- get_data.py
+++ get_data.py
@@ -1,48 +1,70 @@
 import os
 import sys
 import json
-
-
 from dotenv import load_dotenv
-
-
 import certifi
 import pandas as pd
 import numpy as np
-from pymongo 
-import MongoClient
+import pymongo
+from pymongo import MongoClient
+
 from networksecurity.exception import NetworkSecurityException  
 from networksecurity.logger import logging
+
+# Load environment variables first
+load_dotenv()
+
+MONGO_DB_URL = os.getenv("MONGO_URI")
+print(MONGO_DB_URL)
 
 class NetworkDataExtract():
     def __init__(self):
         try:
-            load_dotenv()
-            self.mongo_uri = os.getenv("MONGO_URI")
-            self.mongo_db = os.getenv("MONGO_DB")
-            self.mongo_collection = os.getenv("MONGO_COLLECTION")
-            self.client = MongoClient(self.mongo_uri, tlsCAFile=certifi.where())
-            self.db = self.client[self.mongo_db]
-            self.collection = self.db[self.mongo_collection]
+            # self.mongo_uri = os.getenv("MONGO_URI")
+            # self.mongo_db = os.getenv("MONGO_DB")
+            # self.mongo_collection = os.getenv("MONGO_COLLECTION")
+            # self.client = MongoClient(self.mongo_uri, tlsCAFile=certifi.where())
+            # self.db = self.client[self.mongo_db]
+            # self.collection = self.db[self.mongo_collection]
+            # logging.info("MongoDB connection established successfully")
+            pass
         except Exception as e:
-            raise NetworkSecurityException(e, sys) from e
+            raise NetworkSecurityException(e, sys)
         
-    def csv_to_json(self, csv_file_path: str) -> list:
+    def csv_to_json_convertor(self,file_path):
         try:
-            df = pd.read_csv(csv_file_path)
-            json_data = df.to_dict(orient='records')
-            return json_data
+            data=pd.read_csv(file_path)
+            data.reset_index(drop=True, inplace=True)
+            records=list(json.loads(data.T.to_json()).values())
+            return records
         except Exception as e:
-            raise NetworkSecurityException(e, sys) from e
+            raise NetworkSecurityException(e, sys)
+        
     
-    def pushing_data_to_mongodb(self, data: list):
+    def pushing_data_to_mongodb(self,records, database,collection):
         try:
-            if isinstance(data, list):
-                self.collection.insert_many(data)
-            else:
-                raise NetworkSecurityException("Data should be a list of dictionaries", sys)
+            self.database=database
+            self.collection=collection
+            self.records=records
+            
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            
+            self.database = self.mongo_client[self.database]
+            
+            self.collection=self.database[self.collection]
+                
+            self.collection.insert_many(self.records)
+                
+            return len(self.records)
+            
         except Exception as e:
-            raise NetworkSecurityException(e, sys) from e
+            raise NetworkSecurityException(e, sys)
 
 
-    
+# if __name__ == "__main__":
+#     FILE_PATH = "E:\\MLOPS\\Network_Security\\Network_Data\\network_data.csv"
+#     DATABASE_NAME = "NetworkSecurityDB"
+#     COLLECTION_NAME = "NetworkDataCollection"
+#     data_extractor = NetworkDataExtract()
+#     data = data_extractor.csv_to_json_convertor(FILE_PATH)
+#     data_extractor.pushing_data_to_mongodb(data, DATABASE_NAME, COLLECTION_NAME)
---
+++ networksecurity/logger/__init__.py
@@ -0,0 +1,19 @@
+import logging as logging
+import os
+from datetime import datetime
+
+# Create logs directory
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+# Export logging
+__all__ = ['logging']
