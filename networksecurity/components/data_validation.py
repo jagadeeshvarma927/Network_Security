e192382531d5ca8c13a0e74281f369b57583abc1
jagadeeshvarma927 <jagadeeshvarma927@gmail.com>
2026-02-24 10:28:48 +0530

    data validation added

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -1,8 +1,83 @@
-
+from networksecurity.constant.training_pipeline import SCHEMA_FILE_PATH
+from networksecurity.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from networksecurity.entity.config_entity import DataValidationConfig
+from networksecurity.exception.exception import NetworkSecurityException 
+from networksecurity.logger.logger import logging 
+from networksecurity.utils.main_utils.utils import read_yaml_file,write_yaml_file
+from scipy.stats import ks_2samp
+import pandas as pd
+import os,sys
 
 
 class DataValidation:
     def __init__(self):
         pass
+
+    def validate_number_of_columns(self,df:pd.DataFrame)->bool:
+        try:
+           pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+    def is_numerical_column_exist(self,df:pd.DataFrame)->bool:
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+
+    @staticmethod
+    def read_data(file_path)->pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+
+    def detect_data_drift(self,base_df,current_df,threshold=0.05)->bool:
+        try:
+            status=True
+            report ={}
+            for column in base_df.columns:
+                d1 = base_df[column]
+                d2  = current_df[column]
+                is_same_dist = ks_2samp(d1,d2)
+                if threshold<=is_same_dist.pvalue:
+                    is_found=False
+                else:
+                    is_found = True 
+                    status=False
+                report.update({column:{
+                    "p_value":float(is_same_dist.pvalue),
+                    "drift_status":is_found
+                    
+                    }})
+            
+            drift_report_file_path = self.data_validation_config.drift_report_file_path
+            
+            #Create directory
+            dir_path = os.path.dirname(drift_report_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            write_yaml_file(file_path=drift_report_file_path,content=report,)
+            return status
+            
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+
+    def initiate_data_validation(self)->DataValidationArtifact:
+        try :
+            self.read_data()
+            self.validate_number_of_columns()
+           
+            self.detect_data_drift()
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    
+
+
+        
+    
         
         
